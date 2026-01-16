ac6269300a7b77a37bb9beaffa8dbb9ee90184b8
Mary Andrade <mariadeandrade@hotmail.com.br>
2026-01-15 22:30:44 -0300

    At

---
+++ Estudos/Anotacao.c
@@ -0,0 +1,39 @@
+//## Estudos para a primeira prova Piscina 42
+
+///## Sempre usar
+
+//#include <unistd.h>
+
+void ft_purchar (char c)
+{
+    write (1, &c, 1)
+    1 - terminal
+    & - endereço da variavel
+    1 - saida (output)
+char n = '0'; /// guardando o numero 0 na tabela ASCII
+while (n <= '9') /// Loop para gerar os número em que o n é menor que 9
+Diferentemente de outros em C o caratere se encontra em uma tabela ASCII e na verdade o 0 é (48) na tabela e o 1 é o (49)
+portanto a variável vai rodar para encontrar o número pedido e o loop vai dizer como: "Comece no 48 e vá até o 57".
+{
+    write(1, &n, 1);
+    n++;
+}
+}
+
+
+# include <unistd.h>
+
+void ft_puchar (char c)
+{
+    while
+    (ab, $c ,cd)
+    //assim em diante
+}
+//
+{
+    int main (void)
+
+write(1,"abcdefgh\n",27);
+return(0);
+}
+///
