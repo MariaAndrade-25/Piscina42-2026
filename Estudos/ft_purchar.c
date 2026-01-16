9f8f7720ea7fb611f2b5f71eb09e7e39076c94e5
Mary Andrade <mariadeandrade@hotmail.com.br>
2026-01-16 15:19:43 -0300

    maria

--- Estudos/ft_purchar.c
+++ Estudos/ft_purchar.c
@@ -11,18 +11,16 @@
 #                                                                              #
 # **************************************************************************** #
 
-// Criando um FT ft_purchar
-
 #include <unistd.h>
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
-//*
+///* 
 int	main(void)
 {
 	ft_putchar('A');
 	return (0);
 }
-//*
+///*
